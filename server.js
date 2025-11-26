8fc03345f45a92d4d249569ef6744bf1ac98cf6b
Pedro Augusto de Melo Borges <pedroameloborges@gmail.com>
2025-11-25 21:45:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require("express")
-const fetch = require("node-fetch")
+const express = require("express");
+const fetch = require("node-fetch");
 
-const app = express()
+const app = express();
+
+app.use(express.static("public")); // 
 
 app.get('/proxy', async (req, resp) => {
     try {
         const url = req.query.url;
-        const response = await fetch (url);
+        const response = await fetch(url);
         const text = await response.text();
 
-        resp.set("Access-Control-Allow-Origin", "*")
-        resp.send(text)
+        resp.set("Access-Control-Allow-Origin", "*");
+        resp.send(text);
     }
-    catch (e){
-        resp.status(500).send("Erro" + e.message)
+    catch (e) {
+        resp.status(500).send("Erro " + e.message);
     }
 });
-
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Proxy running on port " + PORT);
 });
-
 
 
