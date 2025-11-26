0787c9340883f5403a232e937aa945c99c1aa687
Pedro Augusto de Melo Borges <pedroameloborges@gmail.com>
2025-11-25 21:17:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const fetch = require("node-fetch")
 
 const app = express()
 
-// Servir os arquivos da pasta public
 app.use(express.static("public"))
 
 app.get('/proxy', async (req, resp) => {
     try {
         const url = req.query.url;
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                "User-Agent":
+                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Accept": "text/html"
+            }
+        });
+
         const text = await response.text();
 
         resp.set("Access-Control-Allow-Origin", "*")
